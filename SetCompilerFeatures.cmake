6e4a697f87a9545ad4df63c31a7610be0c168841
Ivan Shynkarenka <chronoxor@gmail.com>
2016-05-23 06:06:44 -0700

    Update compilers features

--- SetCompilerFeatures.cmake
+++ SetCompilerFeatures.cmake
@@ -1,14 +1,17 @@
-# Enable c++0x, c++11 and other compiler features
+# Enable C++0x/C++11/C++14 and other compiler features
 
 if(NOT MSVC)
   include(CheckCXXCompilerFlag)
+  CHECK_CXX_COMPILER_FLAG("-std=c++14" COMPILER_SUPPORTS_CXX14)
   CHECK_CXX_COMPILER_FLAG("-std=c++11" COMPILER_SUPPORTS_CXX11)
   CHECK_CXX_COMPILER_FLAG("-std=c++0x" COMPILER_SUPPORTS_CXX0X)
-  if(COMPILER_SUPPORTS_CXX11)
+  if(COMPILER_SUPPORTS_CXX14)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
+  elseif(COMPILER_SUPPORTS_CXX11)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-  elseif()
+  elseif(COMPILER_SUPPORTS_CXX0X)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
   else()
-    message(STATUS "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support. Please use a different C++ compiler!")
+    message(STATUS "The compiler ${CMAKE_CXX_COMPILER} has no C++0x/C++11/C++14 support. Please use a different C++ compiler!")
   endif()
 endif()
