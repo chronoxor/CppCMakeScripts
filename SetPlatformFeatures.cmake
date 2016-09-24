a2be2007458add5bc33c73ae22af0f55c44937da
Ivan Shynkarenka <chronoxor@gmail.com>
2016-09-24 15:36:29 +0300

    Update

--- SetPlatformFeatures.cmake
+++ SetPlatformFeatures.cmake
@@ -1,13 +1,4 @@
-# Detect _WIN32_WINNT version
+# Set _WIN32_WINNT version for WIN32 platform
 if(WIN32)
-  macro(GET_WIN32_WINNT version)
-    if (WIN32 AND CMAKE_SYSTEM_VERSION)
-      set(ver ${CMAKE_SYSTEM_VERSION})
-      string(REGEX REPLACE "^([0-9])[.]([0-9]).*" "0\\10\\2" ver ${ver})
-      set(${version} "0x${ver}")
-    endif()
-  endmacro()
-
-  GET_WIN32_WINNT(winnt)
-  add_definitions(-D_WIN32_WINNT=${winnt})
+  add_definitions(-D_WIN32_WINNT=0x601)
 endif()
