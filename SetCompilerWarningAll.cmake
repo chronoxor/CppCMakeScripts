3e6d3d016542e33f9a0344e3f5873a56a622b2c4
Ivan Shynkarenka <chronoxor@gmail.com>
2016-10-09 01:51:37 +0300

    update

--- SetCompilerWarningAll.cmake
+++ SetCompilerWarningAll.cmake
@@ -1,29 +1,30 @@
 # Enable pedantic warning checks for GCC and Visual Studio
 if(CMAKE_MAKE_PROGRAM MATCHES "(MSBuild|devenv|msdev|nmake)")
-  # Ignore some paranoid Visual Studio warnings globally:
-  # C4100: 'identifier' : unreferenced formal parameter
-  # C4250: 'class1' : inherits 'class2::member' via dominance
   set(CMAKE_C_WARNING_LEVEL 4)
   if(CMAKE_C_FLAGS MATCHES "/W[0-4]")
     string(REGEX REPLACE "/W[0-4]" "/W4" CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
   else()
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /W4")
   endif()
-  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /WX /wd4100 /wd4250")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /WX")
   set(CMAKE_CXX_WARNING_LEVEL 4)
   if(CMAKE_CXX_FLAGS MATCHES "/W[0-4]")
     string(REGEX REPLACE "/W[0-4]" "/W4" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
   else()
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4")
   endif()
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /WX /wd4100 /wd4250")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /WX")
+  # C4100: 'identifier' : unreferenced formal parameter
+  # C4250: 'class1' : inherits 'class2::member' via dominance
+  set(COMMON_COMPILE_FLAGS "/wd4100 /wd4250")
 elseif(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_GNUC)
   if(NOT CMAKE_C_FLAGS MATCHES "-Wall$")
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -pedantic -Werror")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Werror")
   endif()
   if(NOT CMAKE_CXX_FLAGS MATCHES "-Wall$")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -pedantic -Werror")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Werror")
   endif()
+  set(COMMON_COMPILE_FLAGS "-pedantic")
 else()
   message(STATUS "Unknown build tool, cannot set compiler warning flags!")
 endif()
