6186d7a1d971269869e8c7664a90d1698097cdcb
Ivan Shynkarenka <chronoxor@gmail.com>
2017-07-25 17:17:05 +0300

    update

--- SetCompilerFeatures.cmake
+++ SetCompilerFeatures.cmake
@@ -1,40 +1,21 @@
 # Compiler features
 
-if(NOT MSVC)
+# Enable C++17 feature
+set(CMAKE_CXX_STANDARD 17)
 
-  # Enable C++0x/C++11/C++14 feature
+# Clang libc++
+if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+  if(NOT APPLE)
+    set(CMAKE_CXX_STANDARD_LIBRARIES "${CMAKE_CXX_STANDARD_LIBRARIES} -lsupc++")
+  endif()
+endif()
+
+# MSVC C++17 workaround
+if(MSVC_VERSION GREATER_EQUAL "1900")
   include(CheckCXXCompilerFlag)
-  CHECK_CXX_COMPILER_FLAG("-std=c++14" COMPILER_SUPPORTS_CXX14)
-  CHECK_CXX_COMPILER_FLAG("-std=c++11" COMPILER_SUPPORTS_CXX11)
-  CHECK_CXX_COMPILER_FLAG("-std=c++0x" COMPILER_SUPPORTS_CXX0X)
-  if(COMPILER_SUPPORTS_CXX14)
-    if (CYGWIN)
-      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=gnu++14")
-    else()
-      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
-    endif()
-  elseif(COMPILER_SUPPORTS_CXX11)
-    if (CYGWIN)
-      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=gnu++11")
-    else()
-      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-    endif()
-  elseif(COMPILER_SUPPORTS_CXX0X)
-    if (CYGWIN)
-      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=gnu++0x")
-    else()
-      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
-    endif()
-  else()
-    message(STATUS "The compiler ${CMAKE_CXX_COMPILER} has no C++0x/C++11/C++14 support. Please use a different C++ compiler!")
+  CHECK_CXX_COMPILER_FLAG("/std:c++latest" MSVC_LATEST)
+  if(MSVC_LATEST)
+    add_compile_options("/std:c++latest")
   endif()
-  
-  # Clang libc++
-  if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
-    if(NOT APPLE)
-      set(CMAKE_CXX_STANDARD_LIBRARIES "${CMAKE_CXX_STANDARD_LIBRARIES} -lsupc++")
-    endif()
-  endif()
-
 endif()
