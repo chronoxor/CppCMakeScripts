cc15aa01f14bc7dbff1eb685dd8a273f74792619
Ivan Shynkarenka <chronoxor@gmail.com>
2017-11-14 22:35:40 +0300

    update

--- SetCompilerFeatures.cmake
+++ SetCompilerFeatures.cmake
@@ -1,7 +1,11 @@
 # Compiler features
 
-# Enable C++17 standard
-set(CMAKE_CXX_STANDARD 17)
+# Enable C++14 or C++17 standard
+if(CYGWIN)
+  set(CMAKE_CXX_STANDARD 14)
+else()
+  set(CMAKE_CXX_STANDARD 17)
+endif()
 
 # Clang libc++
 if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
