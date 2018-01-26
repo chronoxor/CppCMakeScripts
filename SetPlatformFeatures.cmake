b954e807e6b43d0159610c840f0dc40cfb7bc067
Ivan Shynkarenka <chronoxor@gmail.com>
2018-01-26 15:01:12 +0300

    Update

--- SetPlatformFeatures.cmake
+++ SetPlatformFeatures.cmake
@@ -7,6 +7,8 @@ if(WIN32)
 
   # Disable CRT secure warnings
   add_definitions(-D_CRT_SECURE_NO_DEPRECATE)
+  # Disable C++17 codecvt warnings
+  add_definitions(-D_SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING)  
 
   # Windows 10
   add_definitions(-D_WIN32_WINNT=0x0A00)
