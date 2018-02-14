ca96494b05a334553b7ab7ff7278ab83305098c1
Ivan Shynkarenka <chronoxor@gmail.com>
2018-02-14 12:30:01 +0300

    update

--- SetCompilerFeatures.cmake
+++ SetCompilerFeatures.cmake
@@ -8,6 +8,6 @@ else()
 endif()
 
 # Clang libc++
-#if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-#  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
-#endif()
+if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+endif()
