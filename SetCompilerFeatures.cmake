def9995b827d122c9a0163d54413eb4f0ac91a19
Ivan Shynkarenka <chronoxor@gmail.com>
2018-03-17 13:17:51 +0300

    update

--- SetCompilerFeatures.cmake
+++ SetCompilerFeatures.cmake
@@ -3,9 +3,10 @@
 # Enable C++17 standard
 set(CMAKE_CXX_STANDARD 17)
 
-# Cygwin still use C++14 standard
+# Cygwin C++17 workaround
 if(CYGWIN)
   set(CMAKE_CXX_STANDARD 14)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17")
 endif()
 
 # Clang libc++
