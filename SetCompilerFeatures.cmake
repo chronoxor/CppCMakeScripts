6b3ab8904f45d7f65d263558923cffab941d21e3
Ivan Shynkarenka <chronoxor@gmail.com>
2018-04-01 12:21:11 +0300

    update

--- SetCompilerFeatures.cmake
+++ SetCompilerFeatures.cmake
@@ -8,8 +8,8 @@ else()
 endif()
 
 # Clang libc++
-if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+#if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+#  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
 #  if(NOT APPLE)
 #    set(CMAKE_CXX_STANDARD_LIBRARIES "${CMAKE_CXX_STANDARD_LIBRARIES} -lc++abi -lsupc++")
 #  endif()
