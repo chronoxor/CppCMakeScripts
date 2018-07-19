f29fe2af82ab444599b5d897f39dc308a7fdd39f
Ivan Shynkarenka <chronoxor@gmail.com>
2018-07-19 16:35:51 +0300

    update

--- SetCompilerFeatures.cmake
+++ SetCompilerFeatures.cmake
@@ -1,10 +1,4 @@
 # Compiler features
 
 # Choose C++ standard
-if(CMAKE_VERSION VERSION_LESS "3.7")
-  if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=gnu++17")
-  endif()
-else()
-  set(CMAKE_CXX_STANDARD 17)
-endif()
+set(CMAKE_CXX_STANDARD 17)
