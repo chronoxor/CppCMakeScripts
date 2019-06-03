3b5a066b728d36804801ebc9c08742f10159f4ac
Ivan Shynkarenka <chronoxor@gmail.com>
2019-06-03 03:28:17 +0300

    update

--- SetPlatformFeatures.cmake
+++ SetPlatformFeatures.cmake
@@ -2,12 +2,16 @@
 
 if(CYGWIN)
 
+  # Base Windows platform
+  add_definitions(-DWIN32)
+
+  # Windows 10
+  add_definitions(-D_WIN32_WINNT=0x0A00)
+
   # Cygwin sockets
   add_definitions(-D__USE_W32_SOCKETS)
 
-endif()
-
-if(WIN32)
+elseif(WIN32)
 
   # Base Windows platform
   add_definitions(-DWIN32)
