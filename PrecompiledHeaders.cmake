59a0ece61043e3aa2e86a01970269f12403a3012
Ivan Shynkarenka <chronoxor@gmail.com>
2019-06-26 20:32:21 +0300

    update

--- PrecompiledHeaders.cmake
+++ PrecompiledHeaders.cmake
@@ -116,7 +116,7 @@ function( target_precompiled_header pch_target pch_file )
 		# add command to compile precompiled header
 		add_custom_command(
 			OUTPUT "${pch_out}"
-			COMMAND ${CMAKE_CXX_COMPILER} ${CXX_DEFINES} ${CXX_INCLUDES} ${CXX_FLAGS} -x c++-header ${pch_h_in} -o ${pch_out}
+			COMMAND ${CMAKE_CXX_COMPILER} \${CXX_DEFINES} \${CXX_INCLUDES} \${CXX_FLAGS} ${PEDANTIC_COMPILE_FLAGS} -x c++-header ${pch_h_in} -o ${pch_out}
 			DEPENDS "${pch_out_h}" "${pch_opt}"
 			COMMENT "Compiling precompiled header"
 		)
