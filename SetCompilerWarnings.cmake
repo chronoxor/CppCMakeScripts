e68da619862e28f03ee821e45180f1a628e4894c
Ivan Shynkarenka <chronoxor@gmail.com>
2019-06-26 20:52:56 +0300

    update

--- SetCompilerWarnings.cmake
+++ SetCompilerWarnings.cmake
@@ -1,55 +1,55 @@
 # Compiler warnings
 
 # Store origin compile flags
-set(CMAKE_C_FLAGS_ORIGIN "${CMAKE_C_FLAGS}")
-set(CMAKE_CXX_FLAGS_ORIGIN "${CMAKE_CXX_FLAGS}")
+set(CMAKE_C_FLAGS_ORIGIN ${CMAKE_C_FLAGS})
+set(CMAKE_CXX_FLAGS_ORIGIN ${CMAKE_CXX_FLAGS})
 
 # Create custom compile flags
-set(CMAKE_C_FLAGS_CUSTOM "${CMAKE_C_FLAGS}")
-set(CMAKE_CXX_FLAGS_CUSTOM "${CMAKE_CXX_FLAGS}")
+set(CMAKE_C_FLAGS_CUSTOM ${CMAKE_C_FLAGS})
+set(CMAKE_CXX_FLAGS_CUSTOM ${CMAKE_CXX_FLAGS})
 
 if(MSVC)
 
   # Set warnings level 4
   set(CMAKE_C_WARNING_LEVEL 4)
   if(CMAKE_C_FLAGS_CUSTOM MATCHES "/W[0-4]")
-    string(REGEX REPLACE "/W[0-4]" "/W4" CMAKE_C_FLAGS_CUSTOM "${CMAKE_C_FLAGS_CUSTOM}")
+    string(REGEX REPLACE "/W[0-4]" "/W4" CMAKE_C_FLAGS_CUSTOM ${CMAKE_C_FLAGS_CUSTOM})
   else()
-    set(CMAKE_C_FLAGS_CUSTOM "${CMAKE_C_FLAGS_CUSTOM} /W4")
+    set(CMAKE_C_FLAGS_CUSTOM ${CMAKE_C_FLAGS_CUSTOM} "/W4")
   endif()
-  set(CMAKE_C_FLAGS_CUSTOM "${CMAKE_C_FLAGS_CUSTOM} /WX /bigobj")
+  set(CMAKE_C_FLAGS_CUSTOM ${CMAKE_C_FLAGS_CUSTOM} "/WX" "/bigobj")
   set(CMAKE_CXX_WARNING_LEVEL 4)
   if(CMAKE_CXX_FLAGS_CUSTOM MATCHES "/W[0-4]")
-    string(REGEX REPLACE "/W[0-4]" "/W4" CMAKE_CXX_FLAGS_CUSTOM "${CMAKE_CXX_FLAGS_CUSTOM}")
+    string(REGEX REPLACE "/W[0-4]" "/W4" CMAKE_CXX_FLAGS_CUSTOM ${CMAKE_CXX_FLAGS_CUSTOM})
   else()
-    set(CMAKE_CXX_FLAGS_CUSTOM "${CMAKE_CXX_FLAGS_CUSTOM} /W4")
+    set(CMAKE_CXX_FLAGS_CUSTOM ${CMAKE_CXX_FLAGS_CUSTOM} "/W4")
   endif()
 
   # Make all warnings into errors and increases the number of sections that an object file can contain
-  set(CMAKE_CXX_FLAGS_CUSTOM "${CMAKE_CXX_FLAGS_CUSTOM} /WX /bigobj")
+  set(CMAKE_CXX_FLAGS_CUSTOM ${CMAKE_CXX_FLAGS_CUSTOM} "/WX" "/bigobj")
 
   # Common compile flags
   # C4100: 'identifier' : unreferenced formal parameter
   # C4250: 'class1' : inherits 'class2::member' via dominance
-  set(COMMON_COMPILE_FLAGS "/wd4100 /wd4250")
+  set(COMMON_COMPILE_FLAGS "/wd4100" "/wd4250")
 
   # Pedantic compile flags
-  set(PEDANTIC_COMPILE_FLAGS "${COMMON_COMPILE_FLAGS}")
+  set(PEDANTIC_COMPILE_FLAGS ${COMMON_COMPILE_FLAGS})
 
 else()
 
   # Make all warnings into errors
-  set(CMAKE_C_FLAGS_CUSTOM "${CMAKE_C_FLAGS_CUSTOM} -Wall -Werror")
-  set(CMAKE_CXX_FLAGS_CUSTOM "${CMAKE_CXX_FLAGS_CUSTOM} -Wall -Werror")
+  set(CMAKE_C_FLAGS_CUSTOM ${CMAKE_C_FLAGS_CUSTOM} "-Wall" "-Werror")
+  set(CMAKE_CXX_FLAGS_CUSTOM ${CMAKE_CXX_FLAGS_CUSTOM} "-Wall" "-Werror")
 
   # Common compile flags
   set(COMMON_COMPILE_FLAGS "")
 
   # Pedantic compile flags
-  set(PEDANTIC_COMPILE_FLAGS "${COMMON_COMPILE_FLAGS} -Wshadow -pedantic")
+  set(PEDANTIC_COMPILE_FLAGS ${COMMON_COMPILE_FLAGS} "-Wshadow" "-pedantic")
 
 endif()
 
 # Update compile flags
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS_CUSTOM}")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS_CUSTOM}")
+set(CMAKE_C_FLAGS ${CMAKE_C_FLAGS_CUSTOM})
+set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS_CUSTOM})
