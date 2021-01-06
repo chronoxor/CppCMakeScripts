ea1b0794ef6417aa470ef1341f08e8da2e3f9dae
Ivan Shynkarenka <chronoxor@gmail.com>
2021-01-07 02:13:56 +0300

    Fix for build in pthreads

--- SetCompilerFeatures.cmake
+++ SetCompilerFeatures.cmake
@@ -2,3 +2,12 @@
 
 # Choose C++ standard
 set(CMAKE_CXX_STANDARD 17)
+
+# Configure build in pthread library
+if(UNIX)
+  set(CMAKE_THREAD_LIBS_INIT "-lpthread")
+  set(CMAKE_HAVE_THREADS_LIBRARY 1)
+  set(CMAKE_USE_WIN32_THREADS_INIT 0)
+  set(CMAKE_USE_PTHREADS_INIT 1)
+  set(THREADS_PREFER_PTHREAD_FLAG ON)
+endif()
