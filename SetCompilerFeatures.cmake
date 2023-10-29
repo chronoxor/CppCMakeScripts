1855ba013ef6f12fd380729ac59b42d6cc673bfb
Ivan Shynkarenka <chronoxor@gmail.com>
2023-10-29 23:38:49 +0400

    Add Clang -stdlib=libc++ option

--- SetCompilerFeatures.cmake
+++ SetCompilerFeatures.cmake
@@ -7,8 +7,8 @@ set(CMAKE_CXX_EXTENSIONS ON)
 # Configure Clang compiler options
 if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
 
-  # Clang compiler should use libc++
-  add_compile_options(-stdlib=libc++)
+  # Clang link should use libc++
+  add_link_options(-stdlib=libc++)
 
 endif()
 
