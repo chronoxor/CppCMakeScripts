c25d96439450dfbf603ab9641148590f9caadc57
Ivan Shynkarenka <chronoxor@gmail.com>
2023-10-29 23:55:10 +0400

    Add Clang -stdlib=libc++ option

--- SetCompilerFeatures.cmake
+++ SetCompilerFeatures.cmake
@@ -4,6 +4,14 @@
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_EXTENSIONS ON)
 
+# Configure Clang compiler options
+if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+
+  # Clang C++ compiler should use libc++
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+
+endif()
+
 # Configure MSYS2/MinGW-w64 compiler options
 if(MINGW OR MSYS)
 
