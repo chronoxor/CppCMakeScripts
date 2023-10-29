3818f25c71603bdf598ebd4814aeaa926347104a
Ivan Shynkarenka <chronoxor@gmail.com>
2023-10-29 23:32:30 +0400

    Add Clang -stdlib=libc++ option

--- SetCompilerFeatures.cmake
+++ SetCompilerFeatures.cmake
@@ -1,8 +1,27 @@
 # Compiler features
 
 # Choose C++ standard
-set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD 23)
 set(CMAKE_CXX_EXTENSIONS ON)
+
+# Configure Clang compiler options
+if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+
+  # Clang compiler should use libc++
+  add_compile_options(-stdlib=libc++)
+
+endif()
+
+# Configure MSYS2/MinGW-w64 compiler options
+if(MINGW OR MSYS)
+
+  # MinGW-w64 increases the number of sections that an object file can contain
+  add_compile_options(-Wa,-mbig-obj)
+
+  # MinGW-w64 use static link std library
+  add_link_options(-static-libgcc -static-libstdc++)
+
+endif()
 
 # Configure Visual Studio compiler options
 if(MSVC)
@@ -17,17 +36,6 @@ if(MSVC)
   # Visual Studio should correctly reports __cplusplus
   # https://devblogs.microsoft.com/cppblog/msvc-now-correctly-reports-__cplusplus
   add_compile_options(/Zc:__cplusplus)
-
-endif()
-
-# Configure MSYS2/MinGW-w64 compiler options
-if(MINGW OR MSYS)
-
-  # MinGW-w64 increases the number of sections that an object file can contain
-  add_compile_options(-Wa,-mbig-obj)
-
-  # MinGW-w64 use static link std library
-  add_link_options(-static-libgcc -static-libstdc++)
 
 endif()
 
