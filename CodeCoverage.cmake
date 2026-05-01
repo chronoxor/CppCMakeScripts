dcbb70750111c4afcdcdb28dfb0dc49819d72f94
chronoxor <chronoxor@gmail.com>
2026-05-02 01:40:21 +0200

    Add code coverage support (Unix only)

--- CodeCoverage.cmake
+++ CodeCoverage.cmake
@@ -6,8 +6,8 @@ if(CODE_COVERAGE)
   endif()
 
   # Use --coverage flag for both GCC and Clang (equivalent to -fprofile-arcs -ftest-coverage)
-  add_compile_options(--coverage)
-  add_link_options(--coverage)
+  add_compile_options(--coverage -O0)
+  add_link_options(--coverage -O0)
 
   message(STATUS "Code coverage is enabled")
 endif()
